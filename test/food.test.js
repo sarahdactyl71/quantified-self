a981483dddebd52dbb70cb3642b03925228f1ab6
Sarah Kirk <kirkse710@gmail.com>
2017-08-22 11:48:20 -0600

    updates food.test.js to include test for method toHTML

--- test/food.test.js
+++ test/food.test.js
@@ -16,4 +16,21 @@ describe("Food", () => {
       expect(food.calories).to.equal("500")
     })
   })
+
+  describe("functions", () => {
+    describe("getAllFoods", () => {
+      it("returns Food object as HTML", () => {
+        const attrs = {id: 1, name: "pizza", calories: 500}
+        const food = new Food(attrs)
+        const expectedHTML =
+          `<tr class="food-row" data-id=${attrs.id}>
+            <td class="food-name" >${attrs.name}</td>
+            <td class="food-calories" >${attrs.calories}</td>
+            <td class="hidden"><button type="button" class="delete" name="delete">Delete</button></td>
+          </tr>`
+        const resultsHTML = food.toHTML()
+        assert.equal(resultsHTML, expectedHTML)
+      })
+    })
+  })
 })
